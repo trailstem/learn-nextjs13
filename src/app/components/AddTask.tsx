281c0b29cc066ab9940488e75c800ae44e1ec526
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-07-25 08:01:30 +0900

    ベースレイアウト完成

---
+++ src/app/components/AddTask.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddTask = () => {
+  return (
+    <div className='text-gray-700'>AddTask</div>
+  )
+}
+
+export default AddTask
---
+++ src/app/components/TodoList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodoList = () => {
+  return <div className="text-gray-700">TodoL ist</div>;
+};
+  
+export default TodoList;
