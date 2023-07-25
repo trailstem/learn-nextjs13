5f918be58ddb6fb4a8759b429443e1d0df7fd9bc
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-07-25 22:21:49 +0900

    基本的なレイアウト・デザインの雛形完成

--- src/app/components/AddTask.tsx
+++ src/app/components/AddTask.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const AddTask = () => {
   return (
-    <div className='text-gray-700'>AddTask</div>
-  )
-}
+    
+      <form className="mb-4 space-y-3">
+        <input type="text" className="w-full border px-4 py-2 rounded-lg focus:outline-none focus:border-blue-400"/>
+        <button className="text-white w-full px-4 py-2 bg-blue-500 rounded  transform hover:bg-blue-400 hover:scale-95 duration-200">Add Task</button>
+      </form>
+  );
+};
 
-export default AddTask
+export default AddTask;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import AddTask from "./components/AddTask";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200">
-      <h1 className="text-4xl font-bold text-gray-700 -mt-32">Nextjs 13</h1>
+      <h1 className="text-4xl font-bold text-gray-700 -mt-32">Next.js 13</h1>
       <div className="w-full max-w-xl mt-5">
         <div className="w-full px-8 py-6 bg-white shadow-md rounded-md">
           <AddTask />
